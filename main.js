66779c615b207440fa32ab11463c07da4e697ca9
Andr-07 <46841332+Andr-07@users.noreply.github.com>
2019-04-12 15:53:35 +0300

    Готовый кроме % и шага назад

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ let numbers = document.querySelectorAll(".numbers");
 let operations = document.querySelectorAll(".operations");
 let tableButtons = document.querySelector('.tableButtons')
 let inputWindow = document.querySelector("#window");
+let equal = document.querySelector(".equal");
+let clear = document.querySelector(".clear");
+let dot = document.querySelector(".dot");
 let key = false;
 let num1, num2;
-
-tableButtons.addEventListener('click', function(event) {
 let numberOfButton = "";
 let operationOfButton = "";
 
+tableButtons.addEventListener('click', function(event) {
+
+function clearAll() {
+    inputWindow.value = "";
+    num1 = 0;
+    num2 = 0;
+    numberOfButton = "";
+    key = false;
+
+}
+
 function counting(num1,operator,num2) {
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    console.log("*********", num1, operator, num2)
     switch (operator) {
         case "+":
             return num1 + num2;
@@ -30,31 +43,60 @@ function counting(num1,operator,num2) {
             break;
             
         case "%":
-            return num1 % num2;
-            break;      
+            return num2*100/num1;
+            break;   
     }
 }
 
     switch (event.target.className) {
+
+//  **** Формирование чисел **** //
+ 
         case "numbers":
             if (key === false) {
-                numberOfButton = event.target.innerText;
-                inputWindow.value += numberOfButton;
-                num1 = inputWindow.value;
+                numberOfButton += event.target.innerText;
+                num1 = numberOfButton;
+                inputWindow.value = num1;
+
             } else {
-                numberOfButton = event.target.innerText;
-                inputWindow.value += numberOfButton;
-                num2 = inputWindow.value;
+                inputWindow.value = "";
+                numberOfButton += event.target.innerText;
+                num2 = numberOfButton;
+                inputWindow.value = num2;
             }
             console.log(num1, num2, key);
             break;
+
+  // ***** Форимирование операций *****  //
             
-            case "operations":
-            operationOfButton = event.target.innerText;
+        case "operations":
+
             console.log(operationOfButton);
-            key = true;
-            inputWindow.value = "";
+            key = true; // Маркер операций
+            if (num2) {
+                num1 = counting(num1, operationOfButton, num2);
+                inputWindow.value = num1;
+                num2 = undefined;
+                operationOfButton = event.target.innerText;
+            }
+            else operationOfButton = event.target.innerText;
+
+            console.log(")))))", num2);
+            numberOfButton = "";
             break;
+
+        case "equal":
+            inputWindow.value = counting(num1, operationOfButton, num2);
+            console.log("-----------", operationOfButton)
+            break;
+
+        case "clear":
+            clearAll();
+            break;
+
+
+
+
     }
 
 
